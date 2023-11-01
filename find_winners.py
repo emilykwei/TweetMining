f28632e43d2b25e12a2482070185ecbeb6313d65
dyu222 <48658466+dyu222@users.noreply.github.com>
2023-11-01 16:08:19 -0500

    winners

--- find_winners.py
+++ find_winners.py
@@ -30,6 +30,7 @@ def winners(category, text, retweet):
         match1 = re.search(p1, s)
         if match1:
             if doc.similarity(spacy_model(match1.group(2))) > threshold:
+                # print(doc.similarity(spacy_model(match1.group(2))), match1.group(0))
                 # print(match1.group(1),"|", match1.group(2), "|", match1.group(0))
                 if "actor" in category or "actress" in category or "director" in category:
                     for ent in spacy_model(match1.group(1)).ents:
@@ -41,6 +42,8 @@ def winners(category, text, retweet):
                     doc2 = spacy_model(match1.group(1))
                     for chunk in doc2.noun_chunks:
                             win = chunk.text.lstrip() # remove leading white space
+                            if any(token.ent_type_ == "PERSON" for token in chunk):
+                                continue
                             winners[win] += 1
                         # print(chunk.text)
                 # for ent in spacy_model(match1.group(1)).ents:
@@ -51,6 +54,7 @@ def winners(category, text, retweet):
             match1 = re.search(p2, s)
             if match1:
                 if doc.similarity(spacy_model(match1.group(1))) > threshold:
+                    # print(doc.similarity(spacy_model(match1.group(1))), match1.group(0))
                     group = match1.group(2)
                     if "actor" in category or "actress" in category or "director" in category:
                         for ent in spacy_model(match1.group(2)).ents:
@@ -59,21 +63,45 @@ def winners(category, text, retweet):
                         doc2 = spacy_model(match1.group(2))
                         for chunk in doc2.noun_chunks:
                             win = chunk.text.lstrip()
+                            if any(token.ent_type_ == "PERSON" for token in chunk):
+                                continue
                             winners[win] += 1
     for s in text:
         win_helper(s)
     for s in retweet:
         win_helper(s)
-    return winners
 
-x= winners("best screenplay - motion picture", text, retweet)
+    winners = merge(winners)
+    # for x,y in winners.items():
+    #     if y>1:
+    #         print(x,y)
+    winner = ""
+    max_frequency = 0
+    # go through winners and choose top one, maybe apply more filtering?
+    for key, frequency in winners.items():
+        if frequency > max_frequency:
+            if len(key)==1: #why is this even capturing single letters?
+                continue
+            max_frequency = frequency
+            winner = key
+    return winner
+
+# print(winners("best screenplay - motion picture", text, retweet)) #django unchained
+# print(winners("best director - motion picture", text, retweet)) #ben affleck
+# print(winners("best performance by an actress in a television series - comedy or musical", text, retweet)) #lena dunham
+# print(winners("best foreign language film", text, retweet)) #amour
+# print(winners("best performance by an actor in a supporting role in a motion picture", text, retweet)) #christoph waltz
+# print(winners("best performance by an actress in a supporting role in a series, mini-series or motion picture made for television", text, retweet)) #maggie smith
+
+
+
+# x= winners("best performance by an actress in a television series - comedy or musical", text, retweet)
+# x= winners("best foreign language film", text, retweet) # gives argo which is wrong..
+# x= winners("best performance by an actress in a supporting role in a series, mini-series or motion picture made for television", text, retweet) # gives argo which is wrong..
 # print(x)
-x = merge(x)
-for w,count in x.items():
-    if count>1:
-        print(w, count)
 
-# TODO: add retweets
-# update the group to be the actual entity
-# figure out how to make this more accurate...
-# merge dictionary to combine entries
+# TODO:  #add retweets
+# update the group to be the actual entity #done i think
+# figure out how to make this more accurate... ... 
+# merge dictionary to combine entries   # should be good
+# return the top winner?
