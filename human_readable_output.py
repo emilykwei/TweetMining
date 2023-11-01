79544ebfab5ba65142eb48db58a0dc8a085da9b2
KatrinaLiqy <katrina28@126.com>
2023-11-01 16:45:14 -0500

    print human readable

---
+++ human_readable_output.py
@@ -0,0 +1,19 @@
+import spacy
+import utils
+from collections import defaultdict 
+
+
+def human_readable_output(data):
+    
+    print("hosts: ",data["hosts"])
+    for key in data["award_data"].keys():
+        print("Award:",key)
+
+        print("Presenters: ",data["award_data"][key]["presenters"])
+        print("Nominees: ",data["award_data"][key]["nominees"])
+        print("Winner: ",data["award_data"][key]["winner"])
+
+    with open("redcarpet.json", 'r') as f:
+        carpet = json.load(f)
+        for key, value in carpet:
+            print(key,": ",value)
